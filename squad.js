2076875a7a04b0cfc6fe1fdb3bd49333cfd71c22
Akil <akil@youvisit.com>
2017-01-19 17:48:00 -0500

    Introducing the Squads class

--- squad.js
+++ squad.js
@@ -3,54 +3,81 @@ const TYPES = {
     WORKERS: "WORKERS",
 }
 class Party {
+    //Party is a list of units that makes up this group
+    //Squad is a list of units that exist
 	constructor(props) {
-        console.log("Constructor", props);
-        this.party = [];
+        this.partyList = [];
+        this.squad = {};
     }
     setParty(party){
-        this.party = party;
-    }
-    getParty(){
-        return this.party;
-    }
-    getPartyList(){
+        //Setup the party list
         const list = [];
         let obj;
-        for(let index in this.party){
-            obj = this.party[0];
+        let count;
+        for(let index in party){
+            obj = party[index];
             switch( typeof(obj) ){
                 case "object":
-                    console.log("Handle Object");
+                    for(const key in obj){
+                        count = obj[key];
+                        for(let ii = 0; ii < count; ++ii){
+                            list.push(key);
+                        }
+                    }
                     break;
                 case "string":
+                    list.push(obj);
                     break;
             }
             // console.log("Party Member", index, this.party[0], typeof(this.party[0]));
         }
-        return list;
+        this.partyList = list;
+    }
+    getMissingUnits(){
+        const needed = this.partyList.splice(0);
+        let role,
+            index;
+        for(const name in this.squad){
+            role = this.squad[name].role;
+            index = needed.indexOf(role);
+            if(index >= 0){
+                needed.splice(index, 1);
+            }
+        }
+        return needed;
+    }
+    //Required props
+    //name the unit name
+    //role the unit's role
+    addCreep(props){
+        const { name, role } = props;
+        this.squad[name] = {
+            role
+        };
+    }
+    removeCreep(name){
+        delete this.squad[name];
     }
 }
 class Workers extends Party {
 	constructor(props) {
 		super(props);
 
-        console.log();
         this.setParty([
-            { [UNITS.WORKER]: 4 }
+            { [UNITS.WORKER]: 4 },
         ]);
-        console.log("Party List", this.getPartyList());
+        // this.addCreep({
+        //     name: "Johnny",
+        //     role: UNITS.WORKER
+        // });
+        // this.addCreep({
+        //     name: "Julie",
+        //     role: UNITS.WORKER
+        // });
+        // console.log("Party List", this.getParty());
+        // console.log("Units in the squad", this.getUnitNames());
+        // console.log("Missing Units", this.getMissingUnits());
     }
-}
-const HATCHERY = {
-    // party: [
-    	// { UNITS.WORKER: 4 },
-    // ];
-}
-const OUTPOST = {
-
-}
-const SQUAD = {
-
 }
 module.exports = {
     TYPES,
@@ -59,7 +86,6 @@ module.exports = {
         switch(type){
             case WORKERS:
                 return new Workers();
-                break;
         }
     }
 };
